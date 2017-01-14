eac01cacda8a43aaf9e1a129d263aec0b6563f48
junyeongyu <Junyeong.Yu@MyGeorgian.ca>
2017-01-14 02:26:00 -0500

    learning Angular two: finishing routing & separating ts files into css & html files

--- Assignment1/public/app/app.module.ts
+++ Assignment1/public/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent }        from './app.component';
 import { DashboardComponent }  from './dashboard.component';
@@ -13,16 +14,7 @@ import { HeroService }         from './hero.service';
   imports:      [ 
     BrowserModule ,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [ 
     AppComponent,
