1c4a5548093a8c5765c54cb36cf1217f2ed4c3e6
junyeongyu <Junyeong.Yu@MyGeorgian.ca>
2017-01-19 00:03:41 -0500

    add route link in footer

--- Assignment1/public/app/include/footer.component.ts
+++ Assignment1/public/app/include/footer.component.ts
@@ -9,23 +9,23 @@ import { Component } from '@angular/core';
                 <div class="col-lg-12">
                     <ul class="list-inline">
                         <li>
-                            <a href="#">Home</a>
+                            <a routerLink="" routerLinkActive="active" fragment="land">Home</a>
                         </li>
                         <li class="footer-menu-divider">&sdot;</li>
                         <li>
-                            <a href="#about">About Me</a>
+                            <a routerLink="" routerLinkActive="active" fragment="about">About Me</a>
                         </li>
                         <li class="footer-menu-divider">&sdot;</li>
                         <li>
-                            <a href="#projects">Projects</a>
+                            <a routerLink="" routerLinkActive="active" fragment="projects">Projects</a>
                         </li>
                         <li class="footer-menu-divider">&sdot;</li>
                         <li>
-                            <a href="#services">Services</a>
+                            <a routerLink="" routerLinkActive="active" fragment="services">Services</a>
                         </li>
                         <li class="footer-menu-divider">&sdot;</li>
                         <li>
-                            <a href="#contact">Contact Me</a>
+                            <a routerLink="/contact" routerLinkActive="active">Contact Me</a>
                         </li>
                     </ul>
                     <p class="copyright text-muted small">Copyright &copy; Junyeong Yu 2017. All Rights Reserved</p>
